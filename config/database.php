79ef9b2a447e9e8c294c6df822336a562cae7fed
張家瑜 <ava849260123456@gmail.com>
2019-03-28 01:15:49 +0800

    update database

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'personal_blogs'),
+            'username' => env('DB_USERNAME', 'ava666'),
+            'password' => env('DB_PASSWORD', 'ava63182'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
